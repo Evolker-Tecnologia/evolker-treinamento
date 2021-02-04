da3def598f2eb356aa5580c999315a63892c90b6
karennasserdev <58389266+karennasserdev@users.noreply.github.com>
2021-02-04 19:56:49 +0000

    Adicionou comentários no código.

--- react/react-simple-app/src/like_button.js
+++ react/react-simple-app/src/like_button.js
@@ -1,7 +1,9 @@
+//Com o modo estrito, você não pode, por exemplo, usar variáveis ​​não declaradas.
 'use strict';
 
 const e = React.createElement;
 
+//primeiro componente React 
 class LikeButton extends React.Component {
     constructor(props) {
         super(props);
@@ -13,13 +15,23 @@ class LikeButton extends React.Component {
             return 'You liked this.';
         }
 
-        return e(
-            'button',
-            { onClick: () => this.setState({ liked: true }) },
-            'Like'
+        //contando apenas com recursos que são nativamentes suportados pelos navegadores. É por isso que usamos uma chamada de função JavaScript para informar ao React o que exibir:
+
+        // Exibe um "Like" <button> também
+        // return e(
+        //     'button',
+        //     { onClick: () => this.setState({ liked: true }) },
+        //     'Like'
+        //   );
+
+
+        return (
+            <button onClick={() => this.setState({ liked: true })}>
+                Like
+            </button>
         );
     }
 }
 
 const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(<LikeButton></LikeButton>, domContainer);
