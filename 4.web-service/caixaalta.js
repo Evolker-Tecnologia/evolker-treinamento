6e51899851f25468b295c37d9577c4be201268de
karennasserdev <58389266+karennasserdev@users.noreply.github.com>
2021-03-29 19:40:08 +0100

    Criou um web service com as abordagens pedidas

---
+++ 4.web-service/caixaalta.js
@@ -0,0 +1,8 @@
+export function caixaAlta(entr) {
+    let entrada = entr.toUpperCase();
+
+
+    let saída = JSON.parse(entrada);
+
+    return saída;
+}
---
+++ 4.web-service/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+
+// app.get('/texo', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// app.get('/json', (req, res) => {
+//     res.send(res.json(
+
+//         {
+//             "cliente1": {
+//                 "nome": "Joana",
+//                 "senha": "senha1",
+//                 "profissão": "aluna",
+//                 "id": 1
+//             },
+
+//             "cliente2": {
+//                 "nome": "Maria",
+//                 "senha": "senha2",
+//                 "profissão": "professora",
+//                 "id": 2
+//             },
+
+//             "cliente3": {
+//                 "nome": "Ana",
+//                 "senha": "senha3",
+//                 "profissão": "diretora",
+//                 "id": 3
+//             }
+//         }
+
+
+//     ));
+// });
+
+function caixaAlta(entr) {
+    let json = {
+        entrada: entr,
+        saida: entr.toUpperCase()
+    }
+
+    return json;
+}
+
+
+app.get('/converter', (req, res) => {
+
+    res.send(caixaAlta("Karen"));
+});
+
+// Executar na porta execífica ou 8080. 
+//process.env.PORT é uma variável de ambiente definida pelo ambiente de execução do App Engine. 
+//Se o servidor não estiver configurado para fazer detecções nessa porta, ele não receberá solicitações.
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT);
+
+
+
+
