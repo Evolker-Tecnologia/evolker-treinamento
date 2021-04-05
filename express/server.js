52c7d056c37524b084c97cebe14383c7fbe3c040
karennasserdev <58389266+karennasserdev@users.noreply.github.com>
2021-04-05 21:52:57 +0100

    Removeu exemplo de API Users e deixou apenas aAPI Caixa Alta

--- express/server.js
+++ express/server.js
@@ -13,19 +13,6 @@ function caixaAlta(entr) {
     return json;
 }
 
-let users = [
-    "Mateus",
-    "João",
-    "Antonia",
-    "Margarida"
-]
-
-function add(user) {
-    users.push(user);
-    let index = users.indexOf(user);
-    return index;
-}
-
 app.get('/converter', (req, res) => {
     var entrada = req.query.entrada;
     res.send(caixaAlta(entrada));
@@ -35,16 +22,6 @@ app.post('/converter', jsonParser, (req, res) => {
     var texto = req.body.entrada;
     var saida = caixaAlta(texto);
     res.end(JSON.stringify(saida));
-});
-
-app.get('/user', (req, res) => {
-    res.send(users);
-});
-
-app.post('/user', jsonParser, (req, res) => {
-    var novoUser = req.body.nome;
-    var adicionar = add(novoUser);
-    res.end(JSON.stringify(adicionar));
 });
 
 const PORT = process.env.PORT || 8080;
