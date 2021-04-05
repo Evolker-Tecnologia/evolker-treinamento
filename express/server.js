5bcdb2aca9d87ac5693de0070e78b8583d94a87b
karennasserdev <58389266+karennasserdev@users.noreply.github.com>
2021-04-05 21:19:43 +0100

    Removeu comentários e funções sem uso

--- express/server.js
+++ express/server.js
@@ -4,40 +4,6 @@ const app = express();
 const bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
-// app.get('/texo', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-// app.get('/json', (req, res) => {
-//     res.send(res.json(
-
-//         {
-//             "cliente1": {
-//                 "nome": "Joana",
-//                 "senha": "senha1",
-//                 "profissão": "aluna",
-//                 "id": 1
-//             },
-
-//             "cliente2": {
-//                 "nome": "Maria",
-//                 "senha": "senha2",
-//                 "profissão": "professora",
-//                 "id": 2
-//             },
-
-//             "cliente3": {
-//                 "nome": "Ana",
-//                 "senha": "senha3",
-//                 "profissão": "diretora",
-//                 "id": 3
-//             }
-//         }
-
-
-//     ));
-// });
-
 function caixaAlta(entr) {
     let json = {
         entrada: entr,
@@ -47,18 +13,18 @@ function caixaAlta(entr) {
     return json;
 }
 
+let users = [
+    "Mateus",
+    "João",
+    "Antonia",
+    "Margarida"
+]
 
-// app.get('/converter', (req, res) => {
-
-//     res.send(caixaAlta("Karen"));
-// });
-
-//http://localhost:8080/converter?entrada=teste
-
-// app.get('/converter', (req, res) => {
-//     var entrada = req.query.entrada;
-//     res.send(caixaAlta(entrada));
-// });
+function add(user) {
+    users.push(user);
+    let index = users.indexOf(user);
+    return index;
+}
 
 app.get('/converter', (req, res) => {
     var entrada = req.query.entrada;
@@ -66,19 +32,22 @@ app.get('/converter', (req, res) => {
 });
 
 app.post('/converter', jsonParser, (req, res) => {
-    //code to perform particular action.
-    //To access POST variable use req.body()methods.
     var texto = req.body.entrada;
     var saida = caixaAlta(texto);
     res.end(JSON.stringify(saida));
 });
 
+app.get('/user', (req, res) => {
+    res.send(users);
+});
 
-// Executar na porta execífica ou 8080. 
-//process.env.PORT é uma variável de ambiente definida pelo ambiente de execução do App Engine. 
-//Se o servidor não estiver configurado para fazer detecções nessa porta, ele não receberá solicitações.
+app.post('/user', jsonParser, (req, res) => {
+    var novoUser = req.body.nome;
+    var adicionar = add(novoUser);
+    res.end(JSON.stringify(adicionar));
+});
+
 const PORT = process.env.PORT || 8080;
-
 
 app.listen(PORT);
 
