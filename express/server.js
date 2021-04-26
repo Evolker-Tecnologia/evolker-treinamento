9e3722e683271a3034d0d5606012f8bacd69d4a1
Karen Nasser <karennasser.dev@gmail.com>
2021-04-26 22:10:07 +0100

    #151 Criou um protótipo de webservice CRUD

--- express/server.js
+++ express/server.js
@@ -5,48 +5,78 @@ const app = express();
 const bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
-
+//Dados Bd
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'password',
     database: 'mydb',
     multipleStatements: true
 });
 
+//Criar conexão
 mysqlConnection.connect((err) => {
     if (!err)
         console.log('Conexão estabelecida com sucesso');
     else
         console.log('Falha na conexão!' + JSON.stringify(err, undefined, 2));
+
+
 });
 
-
-function caixaAlta(entr) {
-    let json = {
-        entrada: entr,
-        saida: entr.toUpperCase()
-    }
-
-    return json;
-}
-
+//Ler
 app.get('/converter/:entrada', (req, res) => {
-    // var entrada = req.query.entrada;
-    var entrada = req.params['entrada']
-    res.send(caixaAlta(entrada));
+    let result = [];
+    mysqlConnection.query('SELECT * FROM users', (err, rows) => {
+        if (err) throw err;
+
+        rows.forEach((row) => {
+            result.push(`id ${row.id}: ${row.name}`)
+        });
+        res.send(result);
+    });
 });
 
+//Criar
 app.post('/converter/:entrada', jsonParser, (req, res) => {
+
     var texto = req.params['entrada'];
-    var saida = caixaAlta(texto);
-    res.end(JSON.stringify(saida));
+    const user = { name: texto };
+
+    mysqlConnection.query('INSERT INTO users SET ?', user, (err, res) => {
+        if (err) throw err;
+    });
+    res.end(JSON.stringify(user));
 });
 
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT);
 
 
+//Atualizar
+app.put('/converter', jsonParser, function (req, res) {
+    let name = req.body.name;
+    let id = req.body.id;
+    mysqlConnection.query(
+        'UPDATE users SET name = ? Where ID = ?',
+        [name, id],
+        (err, result) => {
+            if (err) throw err;
+
+            console.log(`Mudou ${result.changedRows} Linha(s)`);
+        }
+    );
+});
 
 
+//Deletar
+app.delete('/converter', jsonParser, function (req, res) {
+    let id = req.body.id;
+    mysqlConnection.query(
+        'DELETE FROM users WHERE id = ?', id, (err, result) => {
+            if (err) throw err;
+
+            console.log(`${result.affectedRows} linha(s) deletadas`);
+        }
+    );
+});
