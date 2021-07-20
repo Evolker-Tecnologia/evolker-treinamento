e96e6553971bf613606bc875c57e2a1d924a066b
AntonyOnScript <santhiagoafk@gmail.com>
2021-07-20 19:05:02 -0300

    fez o exercicio dos 2 inputs no vue server

---
+++ _antony/caixa-alta/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <convertCase/>
+</template>
+
+<script>
+import convertCase from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    convertCase
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ _antony/caixa-alta/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="letras">
+    <input v-model="texto" v-on:keyup="converteLetras">
+    <input v-bind:value="textoMaiusculo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'convertCase',
+  data() {
+    return {
+      texto: '',
+      textoMaiusculo: ''
+    }
+  },
+  methods: {
+    converteLetras(){
+      this.textoMaiusculo = this.texto.toUpperCase()
+    }
+  }
+}
+</script>
