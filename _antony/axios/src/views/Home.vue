7cb86042dfc00068be0cb386c26c0c3a508eab09
AntonyOnScript <santhiagoafk@gmail.com>
2021-07-22 09:45:31 -0300

    Usou axios e logou o json

---
+++ _antony/axios/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+const axios = require ('axios')
+
+axios.get('https://gorest.co.in/public/v1/users').then(resposta => console.log(resposta.data))
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
